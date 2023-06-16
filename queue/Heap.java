587173ee32e691fe558c1cf86b96b736d28f2cbc
Vladimir Jaward Hancco Soncco <vhanccos@unsa.edu.pe>
2023-06-16 11:43:57 -0500

    Agrega estructura para Heap

---
+++ queue/Heap.java
@@ -0,0 +1,26 @@
+package heap;
+
+import java.util.ArrayList;
+
+public class Heap<E extends Comparable<E>> {
+
+    private ArrayList<E> heap;
+
+    public Heap() {
+        heap = new ArrayList<E>();
+    }
+
+    public void insert(E element) {
+        
+        
+    }
+
+    public E remove() {
+        
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
+}
