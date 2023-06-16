aa0538069a578f3c0fbd9856a8dad12e575d583b
Joel Isaias Condori Leon <jcondorile@unsa.edu.pe>
2023-06-16 12:37:02 -0400

    Clase PriorityQueueHeap.java agregado

---
+++ queue/PriorityQueueHeap.java
@@ -0,0 +1,26 @@
+package Ejercicio5;
+import java.util.*;
+
+public class PriorityQueueHeap<T> {
+    private List<PriorityItem<T>> heap;
+
+    public PriorityQueueHeap() {
+        heap = new ArrayList<>();
+    }
+    
+    public void enqueue(T x, int p) {
+        
+    }
+    public T dequeue() {
+
+    }
+    
+    public T front() {
+        
+    }
+    
+    public T back() {
+        
+    }
+    
+}
